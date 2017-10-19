636cd00a294df3f2de76d60ba8844e43258c931d
Joanna Cisowska <asiac00@gmail.com>
2017-10-19 13:23:57 +0200

    added plugin

--- gruntfile.js
+++ gruntfile.js
@@ -33,5 +33,5 @@ module.exports = function(grunt) {
 
   // Default task(s).
 
-  grunt.registerTask('default', ['sass', 'imagemin']);
+  grunt.registerTask('default', ['sass', 'imagemin', 'grunt-autowatch']);
 };
